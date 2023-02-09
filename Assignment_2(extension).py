4f0dd0cb509bd20712cc41408c8b48c0020da6e2
pras-annna <124448766+pras-annna@users.noreply.github.com>
2023-02-09 19:53:06 +0530

    Update Assignment_2(extension).py

--- Assignment_2(extension).py
+++ Assignment_2(extension).py
@@ -1,7 +1,8 @@
 m,n=input("enter filename:").split('.')
-if n=='py':print('python')
-if n=='txt':print('text')
-if n=='ppt':print('powerpoint')
-if n=='xlsx':print('excel')
-else:print(n)
+if n=='py':n=('python')
+if n=='txt':n=('text')
+if n=='ppt':n=('powerpoint')
+if n=='xlsx':n=('excel')
+print(n)
+    
     
